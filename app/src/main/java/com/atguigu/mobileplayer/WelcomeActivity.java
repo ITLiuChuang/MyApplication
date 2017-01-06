e765d3b9e102ac6e87fb3f83aaa2dc99b293ebfc
ITLC <1740104885@qq.com>
2017-01-06 18:48:54 +0800

    欢迎页面跳转MainActivity

--- app/src/main/java/com/atguigu/mobileplayer/WelcomeActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer/WelcomeActivity.java
@@ -1,13 +1,50 @@
 package com.atguigu.mobileplayer;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+
+            }
+        }, 2000);
+    }
+
+    /**
+     * 进入主页面
+     */
+    private void startMainActivity() {
+        //1.进入主页面
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        //2.关闭当前页面
+        finish();
+
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
